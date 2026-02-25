63f4465dcc2caddd09554464713f02eaf7f40d34
Jerry Krinock <jerry@sheepsystems.com>
2026-02-25 10:39:47 -0800

    Sync Log now appends number of bytes to the FROM and TO strings, to make it easier to spot a difference between strings that are very long  and/or cobntain nonprinting characters.

--- Chaker.m
+++ Chaker.m
@@ -48,6 +48,12 @@ NSString* const constLeadingWhitespace = @"    " ;
 	return [[self substringFromIndex:2] localizedCaseInsensitiveCompare:[other substringFromIndex:2]] ;
 }
 
+- (NSInteger)bytesInString:(NSString*)s
+{
+    NSData* data = [s dataUsingEncoding:NSUTF8StringEncoding];
+    return [data length];
+}
+
 - (NSString*)localizedUpdateDescriptionFromValue:(id)oldValue
 										 toValue:(id)newValue
 								 fromDisplayName:(NSString*)fromDisplayName {
@@ -67,8 +73,8 @@ NSString* const constLeadingWhitespace = @"    " ;
 		newValue = [(Stark*)newValue name] ;
 	}
 	
-	NSString* sd1 = [oldValue shortDescription] ;
-	NSString* sd2 = [newValue shortDescription] ;
+	NSString* sd1 = [[oldValue shortDescription] stringByAppendingFormat:@" [%ld]", [self bytesInString:oldValue]];
+	NSString* sd2 = [[newValue shortDescription] stringByAppendingFormat:@" [%ld]", [self bytesInString:newValue]];
 	// In BookMacster 0.9.33, shortDescription was modified to
 	// resolve a performance bottleneck.  See the Release Notes
 	// for 0.9.33 and comments in the implementation of
